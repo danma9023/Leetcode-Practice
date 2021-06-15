c10feb7a338d9fcde4c0cecebb96952153da6030
danma9023 <46980656+danma9023@users.noreply.github.com>
2021-06-15 16:29:57 -0600

    Create 387 First Unique Character in a String.py

---
+++ Leetcode solutions/387 First Unique Character in a String.py
@@ -0,0 +1,41 @@
+# 387. First Unique Character in a String
+# Easy
+
+# 3097
+
+# 150
+
+# Add to List
+
+# Share
+# Given a string s, return the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+# Example 1:
+
+# Input: s = "leetcode"
+# Output: 0
+# Example 2:
+
+# Input: s = "loveleetcode"
+# Output: 2
+# Example 3:
+
+# Input: s = "aabb"
+# Output: -1
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 105
+# s consists of only lowercase English letters.
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = collections.Counter(s)
+        for idx, string in enumerate(s):
+            if count[string] == 1:
+                return idx
+        return -1
+        
