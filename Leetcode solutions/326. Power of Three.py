59523594ab69e1e5655491adb85ffc3d3ed8c1cc
danma9023 <46980656+danma9023@users.noreply.github.com>
2021-07-04 16:54:45 -0600

    Create 326. Power of Three.py

---
+++ Leetcode solutions/326. Power of Three.py
@@ -0,0 +1,39 @@
+# 326. Power of Three
+# Easy
+# 29642Add to ListShare
+# Given an integer n, return true if it is a power of three. Otherwise, return false.
+# An integer n is a power of three, if there exists an integer x such that n == 3x.
+ 
+# Example 1:
+# Input: n = 27
+# Output: true
+# Example 2:
+# Input: n = 0
+# Output: false
+# Example 3:
+# Input: n = 9
+# Output: true
+# Example 4:
+# Input: n = 45
+# Output: false
+ 
+# Constraints:
+# •	-231 <= n <= 231 - 1
+ 
+# Follow up: Could you solve it without loops/recursion?
+
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n < 1:
+            return False
+        while n %3 ==0:
+            n/=3
+        return n ==1
+
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n < 1:
+            return False
+        return (math.log10(n)/math.log10(3))%1 ==0
